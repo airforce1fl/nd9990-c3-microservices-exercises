8c163c2f1b6cea5dd584c43c108aac9a2c8fa4c5
airforce1fl <airforce1fl@yahoo.com>
2022-05-14 10:26:46 -0400

    Initial

--- lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
+++ lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
@@ -1,11 +1,11 @@
-
+const DOCKER_USERNAME = process.env.DOCKER_USERNAME
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`${DOCKER_USERNAME} rocks`);
     await sleep(5000);
   }
 }
